566d7fc5283cbc688f0a6f5646a6d1977115d029
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2021-11-10 04:43:27 +0800

    Add block editor edit view

--- includes/block-editor/blocks/search-results/edit.js
+++ includes/block-editor/blocks/search-results/edit.js
@@ -1,24 +1,66 @@
+import Controls from './controls';
+
 /**
- * Internal dependencies
+ * WordPress dependencies
  */
 
-const { __ } = wp.i18n;
 const { useBlockProps } = wp.blockEditor;
+const { __ } = wp.i18n;
+const { useState } = wp.element;
+const { Placeholder, Button } = wp.components;
 
 const Edit = (props) => {
-	const {
-		attributes: {
-			url,
-		},
-		setAttributes,
-	} = props;
+	const { attributes, setAttributes } = props;
 
-	const blockProps = useBlockProps({ className: 'yext-search-results' });
+	const { url } = attributes;
+	const blockProps = useBlockProps();
 
+	const [pageUrl, setPageUrl] = useState(url);
 	return (
 		<>
+			<Controls {...props} />
 			<div {...blockProps}>
-				Test
+				{!url && (
+					<Placeholder
+						icon="none"
+						label={__('Yext Search Results Block', 'yext')}
+						className="wp-block-embed"
+						instructions={__('Add search results url.', 'yext')}
+					>
+						<form>
+							<input
+								type="url"
+								value={pageUrl || ''}
+								className="components-placeholder__input"
+								onChange={(event) => {
+									if (event) {
+										event.preventDefault();
+									}
+									setPageUrl(event.target.value);
+								}}
+							/>
+							<Button
+								isPrimary
+								onClick={() => {
+									setAttributes({ url: pageUrl });
+								}}
+							>
+								{__('Submit', 'yext')}
+							</Button>
+						</form>
+					</Placeholder>
+				)}
+
+				{url && (
+					<iframe
+						title="Yext Search Results"
+						src={url}
+						height="500px"
+						width="100%"
+						frameBorder="0"
+						scrolling="no"
+					/>
+				)}
 			</div>
 		</>
 	);
