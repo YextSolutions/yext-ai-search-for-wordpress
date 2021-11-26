0fc2606be668817a02b86f8f3bda02e781d97797
Juan Pablo Amato <pabamato@gmail.com>
2021-11-25 22:49:36 -0300

    Show current tab after settings save

--- assets/js/admin/components/tabs.js
+++ assets/js/admin/components/tabs.js
@@ -1,8 +1,49 @@
 // Imports
 import tabs from '@10up/component-tabs';
+import { addQueryArgs, getQueryArg, hasQueryArg } from '@wordpress/url';
 
-// eslint-disable-next-line new-cap, no-new
-const init = () => new tabs('#yext-settings .tabs');
+const TAB_QUERY_VAR = 'tab-selected';
+
+/**
+ * Return the index for a tab
+ *
+ * @param  {Object} node DOM node element
+ * @return {number}      Index
+ */
+function getTabIndex(node) {
+	return [...node.parentElement.children].indexOf(node);
+}
+
+const init = () => {
+	const yextForm = document.querySelector('#yext-settings form');
+	// do nothing if not Yext settings form
+	if (!yextForm) {
+		return;
+	}
+	// update the referer hidden field and include the current active tab
+	const refererInputField = yextForm.querySelector('input[name="_wp_http_referer"]');
+	const refererUrl = refererInputField.value;
+	// eslint-disable-next-line new-cap, no-unused-vars
+	window.settingsTabs = new tabs('#yext-settings .tabs', {
+		onCreate: () => {
+			if (!hasQueryArg(window.location.href, TAB_QUERY_VAR)) {
+				return;
+			}
+			const selectedTab = parseInt(getQueryArg(window.location.href, TAB_QUERY_VAR), 10);
+			const tabLinks = yextForm.querySelectorAll('.tab-list [role="tab"]');
+
+			if (selectedTab <= tabLinks.length) {
+				tabLinks[selectedTab].click();
+			}
+		},
+		onTabChange: () => {
+			const currentTab = yextForm.querySelector('.tab-group [aria-hidden="false"]');
+			refererInputField.value = addQueryArgs(refererUrl, {
+				'tab-selected': getTabIndex(currentTab),
+			});
+		},
+	});
+};
 
 // safe to ignore, this is not a react component
 // eslint-disable-next-line @wordpress/no-global-event-listener
