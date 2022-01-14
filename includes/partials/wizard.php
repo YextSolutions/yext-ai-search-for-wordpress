1f3d6485e58fa4862098adddbaa10f87323ca541
Pablo Amato <pablo.amato@get10up.com>
2022-01-14 13:33:06 -0300

    update copy - page 1

--- includes/partials/wizard.php
+++ includes/partials/wizard.php
@@ -127,12 +127,16 @@ $is_live      = isset( $settings['wizard'] ) ? $settings['wizard']['live'] : fal
 									<path d="M360 0C161.18 0 0 161.18 0 360s161.18 360 360 360 360-161.18 360-360S558.82 0 360 0Zm0 691.2C177.08 691.2 28.8 542.92 28.8 360S177.08 28.8 360 28.8 691.2 177.08 691.2 360 542.92 691.2 360 691.2Z" fill="currentColor"/>
 									<path d="M370.8 399.6h64.8v129.6h28.8V399.6h64.8v-28.8H370.8v28.8Zm-38.37-32.4L270 429.64l-62.43-62.44-20.37 20.37L249.64 450l-62.44 62.43 20.37 20.37L270 470.36l62.43 62.44 20.37-20.37L290.36 450l62.44-62.43-20.37-20.37Zm115.77-18c44.73 0 81-36.27 81-81h-28.8c0 28.83-23.37 52.2-52.2 52.2-8.23 0-16.01-1.91-22.93-5.3l90.91-90.91c-14.44-22.25-39.48-36.98-67.98-36.98-44.74 0-81 36.27-81 81s36.26 80.99 81 80.99Zm0-133.2c10.12 0 19.56 2.89 27.56 7.88l-71.88 71.88c-4.99-8-7.87-17.44-7.87-27.56-.01-28.83 23.36-52.2 52.19-52.2ZM270 259.58l-60.74-72.38-22.06 18.51 68.4 81.52v61.97h28.8v-61.97l68.4-81.52-22.06-18.51L270 259.58Z" fill="currentColor"/>
 								</svg>
-								<h1><?php echo esc_html( 'Keyword Search is the past. AI Search is the future.', 'yext' ); ?></h1>
-								<p><?php echo esc_html( 'Welcome to Yext! Build amazing AI search experiences based on natural language understanding and using a multi-algorithm approach.', 'yext' ); ?></p>
+								<h1>
+									<?php echo esc_html__( 'Welcome to Yext AI Search.', 'yext' ); ?>
+								</h1>
+								<p>
+									<?php echo esc_html__( 'Eager to add or improve search on your WordPress site? Use this plugin to embed Answers, Yext’s AI-powered search experience, directly onto your site.', 'yext' ); ?>
+								</p>
 							</div>
 							<div class="yext-settings__card-header">
 								<h2>
-									<?php echo esc_html( 'Do you have a Yext account?', 'yext' ); ?>
+									<?php echo esc_html__( 'Do you have a Yext account?', 'yext' ); ?>
 								</h2>
 							</div>
 							<div class="yext-settings__form-content">
@@ -142,8 +146,8 @@ $is_live      = isset( $settings['wizard'] ) ? $settings['wizard']['live'] : fal
 											<?php
 											echo sprintf(
 												'<strong>%s</strong>, %s',
-												esc_html( 'Yes', 'yext' ),
-												esc_html( ' I have a Yext account', 'yext' )
+												esc_html__( 'Yes', 'yext' ),
+												esc_html__( ' I have a Yext account', 'yext' )
 											);
 											?>
 										</span>
@@ -156,8 +160,8 @@ $is_live      = isset( $settings['wizard'] ) ? $settings['wizard']['live'] : fal
 											<?php
 											echo sprintf(
 												'<strong>%s</strong>, %s',
-												esc_html( 'No', 'yext' ),
-												esc_html( 'I like to set one up', 'yext' )
+												esc_html__( 'No', 'yext' ),
+												esc_html__( 'I need to set one up', 'yext' )
 											);
 											?>
 										</span>
@@ -171,7 +175,9 @@ $is_live      = isset( $settings['wizard'] ) ? $settings['wizard']['live'] : fal
 					</div>
 					
 					<div class="yext-settings__card-footer">
-						<h1><?php echo esc_html( 'Thousands of brands deliver millions of answers with Yext.', 'yext' ); ?></h1>
+						<h1>
+							<?php echo esc_html__( 'Thousands of brands deliver millions of answers with Yext. Hear some of their stories.', 'yext' ); ?>
+						</h1>
 
 						<div class="yext-settings__logo-grid logo-grid">
 							<ul>
@@ -200,6 +206,16 @@ $is_live      = isset( $settings['wizard'] ) ? $settings['wizard']['live'] : fal
 									<img src="<?php echo esc_url( YEXT_URL . '/dist/images/logos/farmers.png' ); ?>" loading="lazy" />
 								</li>
 							</ul>
+							<p>
+								<?php
+								echo sprintf(
+									'%s <a href="%s">%s</a>',
+									esc_html__( 'Have questions or want to learn more?', 'yext' ),
+									'https://hitchhikers-answers.yext.com/',
+									esc_html__( 'Ask Yext!', 'yext' )
+								);
+								?>
+							</p>
 						</div>
 					</div>
 				</div>
@@ -209,7 +225,7 @@ $is_live      = isset( $settings['wizard'] ) ? $settings['wizard']['live'] : fal
 						<div class="yext-settings__card-content yext-settings__card-content--width-75">
 							<div class="yext-settings__card-header">
 								<h2>
-									<?php echo esc_html( 'Do you want to connect this website to Yext’s knowledge graph?', 'yext' ); ?>
+									<?php echo esc_html__( 'Do you want to connect this website to Yext’s knowledge graph?', 'yext' ); ?>
 								</h2>
 								<p>
 									<?php echo esc_html( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt amet amet et ultricies felis mattis parturient vitae sed. Mauris laoreet.', 'yext' ); ?>
