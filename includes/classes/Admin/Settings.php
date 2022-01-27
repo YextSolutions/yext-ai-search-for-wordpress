2fbb9ca7fa8b01de0078f5338aabb6ce572f8d4e
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2022-01-28 06:19:49 +0800

    Formatting fixes

--- includes/classes/Admin/Settings.php
+++ includes/classes/Admin/Settings.php
@@ -168,8 +168,8 @@ final class Settings {
 	 */
 	public function add_plugin_page() {
 
-		$force_skipped  = isset( $_GET['skipped'] ) ? sanitize_text_field( $_GET['skipped'] ) : false;
-		$skipped        = $force_skipped || ( isset( $this->settings['wizard'] ) && isset( $this->settings['wizard']['active'] ) && ! $this->settings['wizard']['active'] );
+		$force_skipped = isset( $_GET['skipped'] ) ? sanitize_text_field( $_GET['skipped'] ) : false;
+		$skipped       = $force_skipped || ( isset( $this->settings['wizard'] ) && isset( $this->settings['wizard']['active'] ) && ! $this->settings['wizard']['active'] );
 
 		add_menu_page(
 			__( 'Yext', 'yext' ),
@@ -364,7 +364,7 @@ final class Settings {
 				return esc_html( $css );
 			}
 		} else {
-			if ( in_array( $key, $pixel_value ) ) {
+			if ( in_array( $key, $pixel_value, true ) ) {
 				$value = $value . 'px';
 			}
 
