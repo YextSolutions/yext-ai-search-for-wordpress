329455edef3b0b12b94c099dac502451def1178c
Burhan Nasir <burhandodhy@gmail.com>
2025-12-04 18:12:55 +0500

    Update apiVersion version to 3 and fix deprecated warnings (#175)

--- includes/block-editor/blocks/search-results/register.php
+++ includes/block-editor/blocks/search-results/register.php
@@ -7,7 +7,7 @@
 
 namespace Yext\Blocks\SearchResults;
 
-use \Yext\Blocks\Block;
+use Yext\Blocks\Block;
 
 /**
  * Register Search Results Block
@@ -18,9 +18,5 @@ function register() {
 	$path = __DIR__;
 	require_once "$path/block.php";
 
-	$n = function( $func ) {
-		return __NAMESPACE__ . "\\$func";
-	};
-
-	new Block( 'search-results', [], $n( 'render' ) );
+	new Block( 'search-results', [], __NAMESPACE__ . '\\render' );
 }
