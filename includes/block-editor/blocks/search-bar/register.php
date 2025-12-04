97a1a7cf7c394b0e850abff87064eaec1ca7603c
Burhan Nasir <burhan.nasir@10up.com>
2025-12-04 16:59:12 +0500

    Cleanup

--- includes/block-editor/blocks/search-bar/register.php
+++ includes/block-editor/blocks/search-bar/register.php
@@ -18,9 +18,5 @@ function register() {
 	$path = __DIR__;
 	require_once "$path/block.php";
 
-	$n = function ( $func ) {
-		return __NAMESPACE__ . "\\$func";
-	};
-
-	new Block( 'search-bar', [], $n( 'render' ) );
+	new Block( 'search-bar', [], __NAMESPACE__ . '\\render' );
 }
